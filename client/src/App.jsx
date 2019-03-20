c004ec460b7ea4f210e928c0f88bcade0847ff20
Nikola Dragan <n.dragan@vegaitsourcing.rs>
2019-03-20 17:04:39 +0100

    test commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import { UserDataContext } from './context/userDataContext';
 import { getFood } from './fake';
 import WelcomePage from './containers/welcome';
 
+//test
 
 function App() {
 	const { authState, authDispatch } = useContext(AuthContext);
